ecbf10414003d4842c309d2678cf1f1740d8bade
Debra Cupitt <debracupitt@gmail.com>
2019-02-26 12:19:09 +1100

    fix title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import "./Header.css";
 function Header(props) {
   return (
     <header className="header">
-      <h1>The G uardian Lookup</h1>
+      <h1>The Guardian Lookup</h1>
     </header>
   );
 }
