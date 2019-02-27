4bf862021a26792494435cc7920f438ad19ecfbf
Debra Cupitt <debracupitt@gmail.com>
2019-02-27 11:18:51 +1100

    Updates

---
+++ precache-manifest.ac3243e248ec1296bf784225df01ec24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7323f41456785dd4543b",
+    "url": "/guardian-search/static/js/runtime~main.7323f414.js"
+  },
+  {
+    "revision": "deacfd2575bb7515c653",
+    "url": "/guardian-search/static/js/main.deacfd25.chunk.js"
+  },
+  {
+    "revision": "cb065fa543b448d14f17",
+    "url": "/guardian-search/static/js/2.cb065fa5.chunk.js"
+  },
+  {
+    "revision": "deacfd2575bb7515c653",
+    "url": "/guardian-search/static/css/main.37b9ebad.chunk.css"
+  },
+  {
+    "revision": "d5532177482b7f4ad82170d487f3e73a",
+    "url": "/guardian-search/index.html"
+  }
+];
